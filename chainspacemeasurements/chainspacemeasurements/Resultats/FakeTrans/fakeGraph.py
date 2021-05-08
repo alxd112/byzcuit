bb65106dd0a24f477fe11eff5799cb64f30225b1
Alex <amoueddene@gmail.com>
2021-05-08 17:09:01 +0200

    Simu for percentage and its graph

--- chainspacemeasurements/chainspacemeasurements/Resultats/FakeTrans/fakeGraph.py
+++ chainspacemeasurements/chainspacemeasurements/Resultats/FakeTrans/fakeGraph.py
@@ -283,3 +283,170 @@ ax1.legend()
 plt.savefig("Graph_var_nbrShard_input.png")
 
 #plt.show()
+
+
+######################
+######################
+
+#fullcross
+filename = "nbrOfShard_medium/fullcross_SH"
+tpsMean = []
+tpsStd  = []
+LatMean = []
+LatStd  = []
+
+for i in range(minSh, maxSh):
+	file = filename+str(i)+"_n2_med.txt"
+	f = open(file, "r")
+	count = 0
+	for line in f:
+		line = line.split(" ")
+		x = ast.literal_eval(line[1])
+
+
+		if count == 0:   tpsMean.append(x[0]*i) 
+		elif count == 1: tpsStd.append(float(line[1]))
+		elif count == 2: LatMean.append(x[0])
+		elif count == 3: LatStd.append(float(line[1]))
+		count += 1
+
+### nocross ###
+filename2 = "nbrOfShard_medium/nofullcross_SH"
+tpsMeanRand = []
+tpsStdRand  = []
+LatMeanRand = []
+LatStdRand  = []
+
+for i in range(minSh, maxSh):
+	file = filename2+str(i)+"_n2_med.txt"
+	f = open(file, "r")
+	count = 0
+	for line in f:
+		line = line.split(" ")
+		x = ast.literal_eval(line[1])
+
+		if count == 0:   tpsMeanRand.append(x[0]*i) 
+		elif count == 1: tpsStdRand.append(float(line[1]))
+		elif count == 2: LatMeanRand.append(x[0])
+		elif count == 3: LatStdRand.append(float(line[1]))
+		count += 1
+
+#fullcross
+filename = "percentage/Clever_"
+clevtpsMean = []
+clevtpsStd  = []
+clevLatMean = []
+clevLatStd  = []
+
+for i in range(minSh, maxSh):
+	file = filename+str(i)+".txt"
+	f = open(file, "r")
+	count = 0
+	for line in f:
+		line = line.split(" ")
+		print(line)
+		if count <= 3:
+			x = ast.literal_eval(line[1])
+
+
+		if count == 0:   clevtpsMean.append(x[0]*i) 
+		elif count == 1: clevtpsStd.append(float(line[1]))
+		elif count == 2: clevLatMean.append(x[0])
+		elif count == 3: clevLatStd.append(float(line[1]))
+		count += 1
+#fullcross
+filename = "percentage/Random_"
+randtpsMean = []
+randtpsStd  = []
+randLatMean = []
+randLatStd  = []
+
+for i in range(minSh, maxSh):
+	file = filename+str(i)+".txt"
+	f = open(file, "r")
+	count = 0
+	for line in f:
+		line = line.split(" ")
+		if count <= 3:
+			x = ast.literal_eval(line[1])
+
+
+		if count == 0:   randtpsMean.append(x[0]*i) 
+		elif count == 1: randtpsStd.append(float(line[1]))
+		elif count == 2: randLatMean.append(x[0])
+		elif count == 3: randLatStd.append(float(line[1]))
+		count += 1
+
+#### fullcross ####
+
+asymetricerrorStd = [clevtpsStd, clevtpsStd]
+
+x = list(range(minSh, maxSh))
+y = clevtpsMean
+print(x)
+print(y)
+fig, (ax0, ax1) = plt.subplots(nrows=2, sharex=True)
+ax0.errorbar(x, y, yerr=asymetricerrorStd, fmt='-o', label = "clever")
+ax0.set_ylabel("Tps [s]")
+ax0.set_title("Graph done with 2 inputs and 2 outputs")
+ax0.grid(True, which = "both")
+
+
+y = clevLatMean
+asymetricerrorLatency = [clevLatStd, clevLatStd]
+ax1.errorbar(x, y, yerr=asymetricerrorLatency, fmt='-o', label = "clever" )
+ax1.set_ylabel("Latency[ms]")
+ax1.set_xlabel("Number of shard")
+ax1.grid(True, which = "both")
+
+#### nocross ####
+
+asymetricerrorStdRand = [randtpsStd, randtpsStd]
+x = [elem+0.01 for elem in x]
+y = randtpsMean
+print(y)
+ax0.errorbar(x, y, yerr=asymetricerrorStdRand, fmt='-x', label = "random")
+
+y = LatMeanRand
+asymetricerrorLatencyRand = [randLatStd, randLatStd]
+ax1.errorbar(x, y, yerr=asymetricerrorLatencyRand, fmt='-x', label = "random")
+
+
+
+
+asymetricerrorStd = [tpsStd, tpsStd]
+
+x = list(range(minSh, maxSh))
+y = tpsMean
+
+ax0.errorbar(x, y, yerr=asymetricerrorStd, fmt='-o', label = "fullcross")
+ax0.set_ylabel("Tps [s]")
+ax0.set_title("Graph done with 2 inputs and 2 outputs")
+ax0.grid(True, which = "both")
+
+
+y = LatMean
+asymetricerrorLatency = [LatStd, LatStd]
+ax1.errorbar(x, y, yerr=asymetricerrorLatency, fmt='-o', label = "fullcross" )
+ax1.set_ylabel("Latency[ms]")
+ax1.set_xlabel("Number of shard")
+ax1.grid(True, which = "both")
+
+#### nocross ####
+
+asymetricerrorStdRand = [tpsStdRand, tpsStdRand]
+x = [elem+0.01 for elem in x]
+y = tpsMeanRand
+print(y)
+ax0.errorbar(x, y, yerr=asymetricerrorStdRand, fmt='-x', label = "nocross")
+
+y = LatMeanRand
+asymetricerrorLatencyRand = [LatStdRand, LatStdRand]
+ax1.errorbar(x, y, yerr=asymetricerrorLatencyRand, fmt='-x', label = "nocross")
+
+
+
+ax0.legend()
+ax1.legend()
+
+plt.savefig("Graphperc_var_nbrShard.png")
